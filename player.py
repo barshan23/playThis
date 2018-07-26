b1c629ced2cd3a2bd622377e941706452a68a08d
Avishek Saha <barshan23@gmail.com>
2018-07-26 12:04:08 +0530

    Added pause-play and full screen funtionality

--- player.py
+++ player.py
@@ -4,14 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
-import time
 
 
 option = webdriver.ChromeOptions()
 option.add_argument('--user-data-dir=/home/barshan23/.config/google-chrome')
 option.add_argument('--start-maximized')
 timeout = 20
-	
+
 app = Flask(__name__)
 browser = None
 
@@ -19,22 +18,45 @@ browser = None
 def index():
 	return render_template('./index.html')
 
-@app.route('/url/<path:url>')
-def play(url):
-	# print url+'?v='+request.args.get('v')
+@app.route('/url', methods=['POST'])
+def play():
 	global browser
-	if 'youtu.be' not in url:
-		url = url+'?v='+request.args.get('v')
+	# if 'youtu.be' not in url:
+	# 	url = url+'?v='+request.args.get('v')
+	url = request.get_json()['url']
+	print url
+	tmode = False
+	if not browser:
+		tmode = True
 
 	browser = webdriver.Chrome(executable_path='./chromedriver', chrome_options=option) if not browser else browser
 	browser.get(url)
-	# find the theater mode button
-	theatre_mode = WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//button[@title='Theater mode']")))
-	# time.sleep(180)
-	print "visible"
-	theatre_mode.click()
-	# name = browser.find_element_by_css_selector('button[title="Theater mode"]')
+	if tmode:
+		# find the theater mode button
+		theatre_mode = WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//button[@title='Theater mode']")))
+		print "visible"
+		theatre_mode.click()
 	return "playing"
+
+
+@app.route('/pp')
+def play_pause():
+	if browser:
+		try:
+			browser.find_element_by_css_selector('button.ytp-play-button').click()
+		except Exception as e:
+			print e
+	return 'done'
+
+
+@app.route('/fscreen')
+def fscreen():
+	if browser:
+		try:
+			browser.find_element_by_css_selector('button.ytp-fullscreen-button').click()
+		except Exception as e:
+			print e
+	return 'done'
 
 @app.route('/stop')
 def stop():
--- static/app.js
+++ static/app.js
@@ -12,8 +12,9 @@
 		};
 		var url = document.getElementById('url').value;
 		console.log(url);
-		request.open('GET', '/url/'+url);
-		request.send();
+		request.open('POST', '/url');
+		request.setRequestHeader("Content-Type", "application/json");
+		request.send(JSON.stringify({url:url}));
 	});
 
 
@@ -28,6 +29,46 @@
 		};
 		request.open('GET', '/stop');
 		request.send();
+	});
+
+	document.getElementById('pause').addEventListener('click',function() {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState === XMLHttpRequest.DONE) {
+				if (request.status === 200) {
+					// stopped
+				}
+			}
+		};
+		request.open('GET', '/pp');
+		request.send();
+		let but = document.getElementById('pause');
+		if (but.innerText === 'Pause') {
+			but.innerText = 'Play'
+		}else {
+			but.innerText = 'Pause'
+		}
+		// console.log(document.getElementById('pause').innerText);
+	});
+
+	document.getElementById('fscreen').addEventListener('click',function() {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState === XMLHttpRequest.DONE) {
+				if (request.status === 200) {
+					// stopped
+				}
+			}
+		};
+		request.open('GET', '/fscreen');
+		request.send();
+		let but = document.getElementById('fscreen');
+		if (but.innerText === 'Fullscreen') {
+			but.innerText = 'Exit Fullscreen'
+		}else {
+			but.innerText = 'Fullscreen'
+		}
+		// console.log(document.getElementById('pause').innerText);
 	});
 
 })();
